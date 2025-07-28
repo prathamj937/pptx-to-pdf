04c2db430d3283024127bf06e79e543d0fa2d4d2
Pratham Jain <117660456+prathamj937@users.noreply.github.com>
2025-07-28 12:25:52 +0530

    Add files via upload

---
+++ pptx_to_pdf/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PptxConverter from './components/PptxConverter';
+
+function App() {
+  return (
+    <div className="App">
+      <PptxConverter />
+    </div>
+  );
+}
+
+export default App;
